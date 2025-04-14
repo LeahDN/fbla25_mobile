13589736fa111a4132cad93f5f0dc5bf5c0808f3
Codr87 <ryhawk07@gmail.com>
2025-04-13 21:31:31 -0500

    Update Crossword1.js

--- Screen/Crossword1.js
+++ Screen/Crossword1.js
@@ -3,14 +3,20 @@ import { View, Text, TextInput,  Button, ScrollView,  Image,  StyleSheet,  Press
 import { styles } from '../Style/Style1';
 import React, { useEffect, useState } from 'react';
 import CrosswordGrid from "../components/CrosswordGrid2";
+import BottomNavBar from '../components/BottomNavBar';
 
 export const CrosswordScreen = ({ route, navigation }) => {
   const { era } = route.params;
   const crosswordData = era.words;
 
   return (
+    <View style={{flex:1}}>
+      <ScrollView 
+        style={styles.detailsContainer}
+        contentContainerStyle={{ paddingBottom: 80 }} // Add padding to     avoid overlap with BottomNavBar
+        showsVerticalScrollIndicator={false}>
     <View style={styles.container}>
-      <Pressable onPress={() => navigation.goBack()} style={styles.backButton}>
+      <Pressable onPress={() => navigation.goBack()} style={styles.backButton2}>
         <Text style={styles.backButtonText}>Back</Text>
       </Pressable>
       {crosswordData ? (
@@ -19,5 +25,10 @@ export const CrosswordScreen = ({ route, navigation }) => {
         <Text>No crossword data available.</Text>
       )}
     </View>
+  </ScrollView>
+  <View style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}></View>
+        <BottomNavBar navigation={navigation} username={route.params.username} />
+      </View>
   );
+
 };
