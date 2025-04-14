0b5ee9379b256b36b26a8673f2fb50a69612fa29
Codr87 <ryhawk07@gmail.com>
2025-04-13 21:34:04 -0500

    Update Info1.js

--- Screen/Info1.js
+++ Screen/Info1.js
@@ -3,6 +3,7 @@
 import { View, Text, TextInput,  Button, ScrollView,  Image,  StyleSheet,  Pressable,} from 'react-native';
 import { styles } from '../Style/Style1';
 import { useEffect, useState } from 'react';
+import {BottomNavBar} from '../components/BottomNavBar'
 
 export const DetailsScreen = ({ route, navigation }) => {
   const [data, setData] = useState([]);
@@ -11,7 +12,42 @@ export const DetailsScreen = ({ route, navigation }) => {
   const { era } = route.params;
 
   return (
-    <ScrollView style={styles.detailsContainer} showsVerticalScrollIndicator={false}>
+   <View style={{ flex: 1 }}>
+      <ScrollView
+        style={styles.detailsContainer}
+        contentContainerStyle={{ paddingBottom: 80 }} // Add padding to avoid overlap with BottomNavBar
+        showsVerticalScrollIndicator={false}
+      >
+        <Pressable onPress={() => navigation.goBack()} style={styles.backButton}>
+          <Text style={styles.backButtonText}>Back</Text>
+        </Pressable>
+        <Text style={styles.modalTitle}>{era.name}</Text>
+        <Image source={{ uri: era.cool_pic }} style={styles.modalImage} />
+        <Text style={styles.modalDescription}>{era.info}</Text>
+        <Pressable
+          style={styles.nextbutton}
+          onPress={() => navigation.navigate("Word", { era })}
+        >
+          <Text style={styles.erabuttontext} title={"Crossword"}>
+            {"Crossword"}
+          </Text>
+        </Pressable>
+        <Pressable
+          style={styles.nextbutton}
+          onPress={() => navigation.navigate("Quiz", { era })}
+        >
+          <Text style={styles.erabuttontext} title={"Quiz"}>
+            {"Quiz"}
+          </Text>
+        </Pressable>
+      </ScrollView>
+      <View style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}></View>
+        <BottomNavBar navigation={navigation} username={route.params.username} />
+      </View>
+    
+  );
+};
+   /* <ScrollView style={styles.detailsContainer} showsVerticalScrollIndicator={false}>
       <Pressable onPress={() => navigation.goBack()} style={styles.backButton}>
         <Text style={styles.backButtonText}>Back</Text>
       </Pressable>
@@ -33,5 +69,12 @@ export const DetailsScreen = ({ route, navigation }) => {
             </Text>
           </Pressable>
     </ScrollView>
+    
+  );
+  return( 
+    <View>
+    <BottomNavBar navigation={navigation} username={route.params.username} />
+    </View>
   );
 };
+*/
