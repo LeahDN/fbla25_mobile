c48794f18e4591ac9ee437316f9fd94e7a39e553
Codr87 <ryhawk07@gmail.com>
2025-04-13 21:32:07 -0500

    Update Home1.js

--- Screen/Home1.js
+++ Screen/Home1.js
@@ -12,6 +12,7 @@ import {
 } from "react-native";
 import { styles } from "../Style/Style1";
 import { useEffect, useState } from "react";
+import BottomNavBar from "../components/BottomNavBar";
 
 export const HomeScreen1 = ({ navigation, route }) => {
   const { username } = route.params;
@@ -37,30 +38,34 @@ export const HomeScreen1 = ({ navigation, route }) => {
   }, []);
 
   return (
-    <ScrollView
-      style={styles.detailsContainer}
-      showsVerticalScrollIndicator={false}
-    >
-      <View style={styles.signOutButtonContainer}>
-        <Pressable style={styles.signOutButton} onPress={handleSignOut}>
-          <Text style={styles.signOutButtonText}>Sign Out</Text>
-        </Pressable>
-      </View>
-      <Text style={styles.welcomeMessage}>Hello, {username}!</Text>
-      <Text style={styles.historyTitle}>History Timeline</Text>
-      {data.map((era, index) => (
-        <View key={index} style={styles.timelineItem}>
-          <Image source={{ uri: era.cool_pic }} style={styles.image} />
-          <Pressable
-            style={styles.erabutton}
-            onPress={() => navigation.navigate("Details", { era })}
-          >
-            <Text style={styles.erabuttontext} title={era.name}>
-              {era.name}
-            </Text>
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        style={styles.detailsContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.signOutButtonContainer}>
+          <Pressable style={styles.signOutButton} onPress={handleSignOut}>
+            <Text style={styles.signOutButtonText}>Sign Out</Text>
           </Pressable>
         </View>
-      ))}
-    </ScrollView>
+        <Text style={styles.welcomeMessage}>Hello, {username}!</Text>
+        <Text style={styles.historyTitle}>History Timeline</Text>
+        {data.map((era, index) => (
+          <View key={index} style={styles.timelineItem}>
+            <Image source={{ uri: era.cool_pic }} style={styles.image} />
+            <Pressable
+              style={styles.erabutton}
+              onPress={() => navigation.navigate("Details", { era, username })}
+            >
+              <Text style={styles.erabuttontext} title={era.name}>
+                {era.name}
+              </Text>
+            </Pressable>
+          </View>
+        ))}
+        <View style={styles.gap2}/>
+      </ScrollView>
+      <BottomNavBar  navigation={navigation} username={username}/>
+    </View>
   );
 };
